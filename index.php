95521917ef9c8546ced920ca8dd162136a87a4fd
thanamongkon <59908382+thanamongkon@users.noreply.github.com>
2020-01-22 14:58:51 +0700

    superlast

--- index.php
+++ index.php
@@ -7,12 +7,12 @@
 <?php
 echo "Hello World!";
 echo "Thanamongkon 62310022";
-<br>
+<br>;
 echo "Phongsakorn 62310134";
-<br>
-echo "Phongporn 62310071";<br>
-echo "Peerapat 62310020";<br>
-echo "Kittisak 62310133";<br>
+<br>;
+echo "Phongporn 62310071";<br>;
+echo "Peerapat 62310020";<br>;
+echo "Kittisak 62310133";<br>;
 ?>
 
 </body>
