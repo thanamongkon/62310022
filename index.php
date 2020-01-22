bee7cce37394ecdfe11a4eb2675f08e2a083e75f
thanamongkon <59908382+thanamongkon@users.noreply.github.com>
2020-01-22 15:01:53 +0700

    lastone

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
 
 <?php
 echo "Hello World!";
-echo "Thanamongkon 62310022"<br>;
-echo "Phongsakorn 62310134"<br>;
-echo "Phongporn 62310071"<br>;
-echo "Peerapat 62310020"<br>;
-echo "Kittisak 62310133"<br>;
+echo "Thanamongkon 62310022 <br>";
+echo "Phongsakorn 62310134<br>";
+echo "Phongporn 62310071<br>";
+echo "Peerapat 62310020<br>";
+echo "Kittisak 62310133<br>";
 ?>
 
 </body>
